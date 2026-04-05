2dac811d98cc66347c5455af89fae5e4b8772ba3
mingyu cao <m.cao@surrey.ac.uk>
2026-04-05 09:48:51 +0800

    add

---
+++ LLaDA/eval_gsm8k.wino.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name="soar_eval"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1                # 请求2块GPU
+#SBATCH --time=24:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate soar
+
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_DATASETS_OFFLINE=0
+export CUDA_VISIBLE_DEVICES=0
+#  --limit 256
+length=256
+
+CUDA_VISIBLE_DEVICES=0 python eval.py --config ./configs/gsm8k.yaml &> wino-gsm8k-ns0-${length}.log
